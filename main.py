35ca796f0f90bc8bfb63661fc4702db2924ce01e
fjjt3 <fjjt3@yahoo.com>
2023-10-16 15:02:12 +0200

    connection a mongoDB

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import requests
 """ requests.post('http://127.0.0.1:5000/product-create',
 data={'name': 'iPhone 5S', 'price': '549.0', 'category': 'Phones'}) """
 
-requests.post('http://127.0.0.1:5000/product-create',
-data={'name': 'iPhone 13', 'price': '799.0', 'category': 'Phones'})
+""" requests.post('http://127.0.0.1:5000/product-create',
+data={'name': 'iPhone 13', 'price': '799.0', 'category': 'Phones'}) """
 
+# mongo request
+
+requests.post('http://127.0.0.1:5000/product-create',
+data={'key': 'iphone-5s', 'name': 'iPhone 5S', 'price': '549.0'})
